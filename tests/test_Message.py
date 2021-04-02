410fa119a6a246c6c459ea82ae5a1c3f9a3833ec
Gary Tse <tse.rong.gary@gmail.com>
2021-04-02 10:53:52 +0800

    test: persist chain state by scoping fixture to module

--- tests/test_Message.py
+++ tests/test_Message.py
@@ -5,7 +5,7 @@ from brownie import accounts
 TOKEN_NAME = 'Immutable Message Version 0'
 TOKEN_SYMBOL = 'IMESSAGE0'
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def MessageNFTContract(Message, accounts):
     yield Message.deploy(TOKEN_NAME, TOKEN_SYMBOL, {'from': accounts[0]})
 
@@ -15,5 +15,16 @@ def test_message_creation(MessageNFTContract, accounts):
 
     assert MessageNFTContract.balanceOf(accounts[1]) == 1
     assert MessageNFTContract.ownerOf(0) == accounts[1]
+    assert MessageNFTContract.viewMessage(0) == 'Hello World!'
+    assert MessageNFTContract.viewMessageCreator(0) == accounts[0]
+
+
+def test_message_transfer(MessageNFTContract, accounts):
+
+    tx1 = MessageNFTContract.transferFrom(accounts[1], accounts[2], 0, {'from': accounts[1]})
+
+    assert MessageNFTContract.balanceOf(accounts[1]) == 0
+    assert MessageNFTContract.balanceOf(accounts[2]) == 1
+    assert MessageNFTContract.ownerOf(0) == accounts[2]
     assert MessageNFTContract.viewMessage(0) == 'Hello World!'
     assert MessageNFTContract.viewMessageCreator(0) == accounts[0]
