99d0739895b9ba065c96d927cefe3576ebacc165
Gary Tse <tse.rong.gary@gmail.com>
2021-04-04 15:16:34 +0800

    test: add events

--- tests/test_Message.py
+++ tests/test_Message.py
@@ -4,6 +4,7 @@ from brownie import accounts
 
 TOKEN_NAME = 'Immutable Message Version 0'
 TOKEN_SYMBOL = 'IMESSAGE0'
+ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 @pytest.fixture(scope="module")
 def MessageNFTContract(Message, accounts):
@@ -18,6 +19,17 @@ def test_message_creation(MessageNFTContract, accounts):
     assert MessageNFTContract.viewMessage(0) == 'Hello World!'
     assert MessageNFTContract.viewMessageCreator(0) == accounts[0]
 
+    assert len(tx1.events) == 2
+
+    # Check Transfer event
+    assert tx1.events[0]['tokenId'] == 0
+    assert tx1.events[0]['sender'] == ZERO_ADDRESS
+    assert tx1.events[0]['receiver'] == accounts[1]
+
+    # Check MessageCreated event
+    assert tx1.events[1]['message'] == 'Hello World!'
+    assert tx1.events[1]['sender'] == accounts[0]
+
 
 def test_message_transfer(MessageNFTContract, accounts):
 
@@ -28,3 +40,8 @@ def test_message_transfer(MessageNFTContract, accounts):
     assert MessageNFTContract.ownerOf(0) == accounts[2]
     assert MessageNFTContract.viewMessage(0) == 'Hello World!'
     assert MessageNFTContract.viewMessageCreator(0) == accounts[0]
+
+    # Check Transfer event
+    assert tx1.events[0]['tokenId'] == 0
+    assert tx1.events[0]['sender'] == accounts[1]
+    assert tx1.events[0]['receiver'] == accounts[2]
